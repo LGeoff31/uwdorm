56d5686b53f9ba62728468dae59a4a575bd362be
LGeoff31 <geoffrey31415@gmail.com>
2024-04-27 14:31:51 -0400

    fixed errors no a tag use link

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -55,23 +55,24 @@ const Navbar = () => {
   return (
     <nav className="bg-white">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link
+          href="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
           <img
             className="h-30 w-20 max-w-xs transition duration-350 ease-in-out hover:shadow-lg dark:hover:shadow-black/20"
             src={"./logo.png"}
           />
-        </a>
+        </Link>
         <div className="text-med w-full md:block md:w-auto md:bg-white">
           <ul className="font-medium flex flex-col p-3 mt-4  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             <li>
-              <a href="/dorms" className="hover:text-blue-600 text-gray-400">
-                Dorms
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="hover:text-blue-600 text-gray-400">
+              <Link
+                href="/contact"
+                className="hover:text-blue-600 text-gray-400"
+              >
                 Contact
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
