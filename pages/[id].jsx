a9a2d5d723233ea6e36d2dc42801c9d893adde23
LGeoff31 <geoffrey31415@gmail.com>
2024-04-28 14:15:13 -0400

    residence page

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Navbar from "./components/Navbar";
+import { Stack, Grid, Box, Typography } from "@mui/material";
 
 const Residence = () => {
   const router = useRouter();
@@ -35,10 +36,56 @@ const Residence = () => {
   }
   return (
     <>
-      <Navbar />
-      {/* <div>{residence[0] && <div>hell {} </div>}</div> */}
-      <img src={residence[0].images} alt="image" />
-      {residence && <div>hello {residence[0].address} adwa</div>}
+      <Grid
+        sx={{
+          background: "rgb(225 246 255)",
+          height: "100vh",
+          position: "relative",
+        }}
+      >
+        <Navbar />
+
+        <div style={{ position: "relative", width: "100%" }}>
+          <img
+            src={residence[0].images}
+            alt="image"
+            style={{ width: "100%", height: "400px" }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              background: "rgba(0, 0, 0, 0.5)",
+            }}
+          />
+        </div>
+
+        <Grid container direction="row" justifyContent={"space-between"}>
+          <Stack padding="2rem" paddingLeft="10rem">
+            <Typography variant="h2"> {residence[0].name}</Typography>
+            <Typography variant="h2">⭐ {3.6}</Typography>
+
+            <Typography variant="h3">Rating Breakdown </Typography>
+
+            <Typography>
+              Room &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;⭐⭐⭐⭐⭐{" "}
+            </Typography>
+            <Typography>Building &nbsp;&nbsp;&nbsp;&nbsp;⭐⭐ </Typography>
+            <Typography>Location &nbsp;&nbsp;&nbsp;⭐⭐⭐⭐⭐ </Typography>
+            <Typography>Bathroom &nbsp;⭐⭐⭐⭐ </Typography>
+            <Typography variant="h2">Description</Typography>
+            <Typography>{residence[0].description}</Typography>
+          </Stack>
+          <Stack padding="2rem" paddingRight="10rem">
+            <Typography variant="h2">Images</Typography>
+            <Typography>{residence[0].description}</Typography>
+            <Typography variant="h2">Comments</Typography>
+          </Stack>
+        </Grid>
+      </Grid>
     </>
   );
 };
