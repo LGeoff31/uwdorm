8a4beef5c9f6a0d2d367eabff579bd3db6663e0c
James Yu <james.yuu@hotmail.com>
2024-04-29 01:14:50 -0400

    styled contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,51 @@
 import Navbar from "./components/Navbar"
+import { Stack } from "@mui/material"
+import img from '../public/top_img.jpg'
 
-export default function contact(){
-    return(
+export default function contact() {
+    return (
         <>
-            <Navbar/>
-            <h1 className="p-5">For more info contact james.yuu@hotmail.com</h1>
-            <h1 className="text-right">hi</h1>
+            <Navbar />
+            <div
+                style={{
+                    background: `url('https://www.theme-junkie.com/wp-content/uploads/After-Effects-Background-Template-20.jpg') no-repeat center center fixed`,
+                    backgroundSize: "cover",
+                    height: "25vh",
+                    display: "flex",
+                
+                }}
+            >
+                <Stack margin="" >
+                    <span className="text-4xl p-20 ml-20 text-blue-300">
+                        About UW Rez
+                    </span>
+                </Stack>
+            </div>
+                
+                <div className="p-20 ml-20"><div className="text-3xl font-bold">Welcome to UW Rez!:</div>
+            
+                <br></br>
+                <h1 className="">UW Rez is a platform website for University of Waterloo incoming students and current students. 
+                You can find every on-campus residence through navigating our website!
+                
+                
+                Our goal is to help students decide where to live based off the experiences of others and ratings
+                for different campus housing.
+                </h1>
+                </div>
+                <div>
+                    <div className="px-20 ml-20 font-bold text-3xl">Our Team:</div>
+                    <img alt="James Yu Image"className=" px-20 py-5 ml-20 w-[22rem] h-[11rem] rounded-full" src="https://prep4collegetutors.com/wp-content/uploads/2023/07/image2-e1688790500290.png"></img>
+                    <h1 className="font-bold text-lg px-20 ml-20">James Yu</h1>
+                    <h1 className="px-20 ml-20">Founder - Biomedical Engineering 2028</h1>
+                    <h1 className="px-20 py-4 ml-20"> James can often be found playing table tennis and at the gym. He coded some backend of the website, but most designed frontend components and code</h1>
+                    <img alt="Geoffrey Lee Image"></img>
+                    <h1 className="font-bold text-lg px-20 ml-20">Geoffrey Lee</h1>
+                    <h1 className="px-20 ml-20">Founder - Software Engineering 2028</h1>
+                    <h1 className="px-20 py-4 ml-20"> ...</h1>            
+                </div>
+            
+
         </>
     )
 }
