32843900a7571fb51740d97fddb497b4cb9c4c8d
James Yu <james.yuu@hotmail.com>
2024-05-04 23:33:02 -0400

    a

--- pages/components/Images.jsx
+++ pages/components/Images.jsx
@@ -512,7 +512,7 @@ const Images = ({ id }) => {
   };
 
   return (
-    <div className="w-[60rem] rounded-sm">
+    <div className="w-[40rem] rounded-sm">
       <AutoPlaySwipeableViews
         axis={theme.direction === "rtl" ? "x-reverse" : "x"}
         index={activeStep}
@@ -537,7 +537,7 @@ const Images = ({ id }) => {
         ))}
       </AutoPlaySwipeableViews>
       <MobileStepper
-        className="rounded-b-2xl w-[50rem]"
+        className="rounded-b-2xl w-[40rem]"
         steps={maxSteps}
         position="static"
         activeStep={activeStep}
