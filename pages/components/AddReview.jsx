29c4faa981cb3dd903fd05fd697f407456ecac40
James Yu <james.yuu@hotmail.com>
2024-05-06 18:10:48 -0400

    added toast

--- pages/components/AddReview.jsx
+++ pages/components/AddReview.jsx
@@ -41,7 +41,24 @@ const AddReview = ({
   if (!id) {
     return <div>Loading</div>;
   }
-
+  const showToast = (message) => {
+    const toast = document.createElement("div");
+    toast.innerHTML = `
+      <div id="toast-success" class="fixed top-4 right-4 z-50 flex items-center w-max p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+        <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+          </svg>
+          <span class="sr-only">Check icon</span>
+        </div>
+        <div class="ml-3 text-sm font-normal">${message}</div>
+      </div>
+    `;
+    document.body.appendChild(toast);
+    setTimeout(() => {
+      toast.remove();
+    }, 4000);
+  };
   const handleSubmit = async () => {
     const data = {
       residence_id: id,
@@ -67,9 +84,10 @@ const AddReview = ({
       console.log("error on inserting comment", response.statusText);
     }
     const p = await response.json();
-
+    showToast("Thanks so much for leaving a comment!")
     handleClose();
     fetchComments();
+
   };
   const handleOpen = () => {
     setOpen(true);
