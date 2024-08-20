9b1e0b2032b8504b27a98832d5695d3e43d6338e
James Yu <james.yuu@hotmail.com>
2024-08-20 12:34:54 -0400

    Added all ai componenets with redirect url and multiple dialogs

--- pages/components/FindResidence.tsx
+++ pages/components/FindResidence.tsx
@@ -1,14 +1,60 @@
 import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
 import { useState } from 'react';
+import ResultDialog from './ResultDialog';
 
 const FindResidence = ()  => {
-  let [isOpen, setIsOpen] = useState<boolean>(false);
-  let [inputText, setInputText] = useState<string>("");
-  let [res, setRes] = useState<string>("");
+  const rooms = [
+    { id: 1, type: 'Traditional Dormitory' },
+    { id: 2, type: 'Suite-Style' },
+    { id: 3, type: 'Apartment-Style' },
+    { id: 4, type: 'Specialty Housing' },
+  ];  
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(event.target.value);
+  const mealplans = [
+    { id: 1, name: 'Yes'},
+    { id: 2, name: 'No'}
+  ]
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isResultOpen, setIsResultOpen] = useState<boolean>(false);
+  const [room, setRoom] = useState<string>("");
+  const [mealplan, setMealplan] = useState<string>("");
+  const [amenities, setAmenities] = useState<string>("");
+  const [details, setDetails] = useState<string>("");
+  const [idealRes, setIdealRes] = useState<string>("");
+  const [similarity, setSimilarity] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  
+  const handleRoomChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setRoom(event.target.value);
+  }
+
+  const handleAmenitiesChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setAmenities(event.target.value);
   };
+
+  const handleDetailsChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDetails(event.target.value);
+  };
+
+  const handleMealplanChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setMealplan(event.target.value)
+  }
+
+  const createInputText = (room: string, mealplan: string, amenities: string, details: string) => {
+    let input_text: string = "";
+    input_text += `The user is looking for a residence that accomodates the room type: ${room}. `
+    if (mealplan == 'No') {
+      input_text += `The user is looking for a residence without mealplan. This could mean they want to cook often and need a residence with ` 
+      + `a cooking area or they are looking to eat out often at different places. `
+    } else {
+      input_text += `The user is looking for a place with a mealplan so that they can eat in the cafeteria. `
+    }
+    input_text += `Moreover, the user wants the residence to have these things available: ${amenities}. `
+    input_text += `Overall, the user is looking for this type of residence to spend their year in: ${details}`
+    console.log('Input text is:', input_text)
+    return input_text
+  }
 
   function open() {
     setIsOpen(true);
@@ -17,6 +63,15 @@ const FindResidence = ()  => {
   function close() {
     setIsOpen(false);
   }
+
+  function openResult() {
+    setIsResultOpen(true)
+  }
+
+  // function closeResult(isResultOpen: boolean) {
+  //   setIsResultOpen(false)
+  // }
+
   const showToast = (message: string) => {
     const toast = document.createElement("div");
     toast.innerHTML = `
@@ -36,31 +91,107 @@ const FindResidence = ()  => {
     }, 4000);
   };
 
-  const handleSubmit = async (event: React.FormEvent) => {
+  const handleAddResidences = async (event: React.FormEvent) => {
     event.preventDefault();
-    if (inputText === "") {
-      window.alert('Please enter a description of your ideal rez!')
-      return;
-    }
-    try {
-      const res = await fetch('/api/find_residence', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ inputText }),
-      });
+  
+    if (room && mealplan && amenities && details) {
+      const inputText = await createInputText(room, mealplan, amenities, details);
+      try {
+        const res = await fetch('/api/insert_residences', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          // body: JSON.stringify({ ideal_res: room, res_des: des, user_des: des1 }),
+        });
+        let selectRoom = document.getElementById('select_room') as HTMLSelectElement;
+        let selectMealplan = document.getElementById('select_mealplan') as HTMLSelectElement;
 
-      const data = await res.json();
-      setRes(data)
-      console.log(data)
-      // showToast(`The ideal residence for you is ${data}`)
-    } catch (error) {
-      console.log(error)
+        if (selectRoom && selectMealplan) {
+          selectRoom.value = 'default';
+          selectMealplan.value = 'default';
+        }
+
+        setRoom("");
+        setMealplan("");
+        setDetails("");
+        setAmenities("");
+        const data = await res.json();
+        console.log(data)
+
+        setIsOpen(false)
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      console.log('unable to create inputText')
+      return;
     }
   };
 
+  const handleFindResidences = async (event: React.FormEvent) => {
+    event.preventDefault();
+  
+    if (room && mealplan && amenities && details) {
+      setIsLoading(true)
+      const inputText = await createInputText(room, mealplan, amenities, details);
+      try {
+        const res = await fetch('/api/find_residence', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ inputText }),
+        });
+        let selectRoom = document.getElementById('select_room') as HTMLSelectElement;
+        let selectMealplan = document.getElementById('select_mealplan') as HTMLSelectElement;
+
+        if (selectRoom && selectMealplan) {
+          selectRoom.value = 'default';
+          selectMealplan.value = 'default';
+        }
+
+        setRoom("");
+        setMealplan("");
+        setDetails("");
+        setAmenities("");
+        const data = await res.json();
+
+        if (data) {
+          const idea = data.results[0].idea
+          const similarity: number = Math.round(data.results[0].similarity * 100)
+          const res_name = parseResidenceFromResults(idea)
+          setIdealRes(res_name)
+          setSimilarity(similarity)
+        }
+        setIsLoading(false)
+        setIsOpen(false)
+        openResult();
+        // showToast(`The ideal residence for you is ${data}`)
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      console.log('unable to create inputText')
+      return;
+    }
+  }
+
+  const parseResidenceFromResults = (results: string) => {
+    let i = 0
+    let res_name = ""
+    while (results[i] !== ',') {
+      if (results[i] === '(') {
+        res_name = res_name.substring(0, res_name.length - 1)
+        break;
+      }
+      res_name += results[i]
+      i++;
+    }
+    return res_name;
+  }
   return (
+    <>
     <label className="flex text-med justify-center h-[3rem] mt-1">
       <button className="text-center rounded-lg bg-gray-300 font-semibold p-3 duration-300 ease-in-out hover:scale-110" onClick={open}>
         Discover your ideal residence!
@@ -71,31 +202,79 @@ const FindResidence = ()  => {
             <DialogPanel
               transition
               className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
-            >
-              <DialogTitle as="h3" className="text-base/7 font-medium text-white">
-                Please enter a description of your ideal residence - include what you are studying, whether you are undergrad or graad, what type of room you are looking 
-                for, what you would like to live close to, and what type of activites and events you are looking forward to attending!
-              </DialogTitle>
-              <input 
-                type='text'
-                value={inputText}
-                onChange={handleInputChange}
+            >                
+                <h1 className='mt-2 text-white mb-1'>What type of room are you looking for?</h1>
+                <label className="flex text-med justify-start h-[2rem]">
+                  <select
+                    className="font-medium rounded-lg bg-gray-300 text-gray-500"
+                    onChange={handleRoomChange}
+                    id="select_room"
+                    value={room}
+                  >
+                    <option value="" disabled hidden>&nbsp;Room type</option>
+                    {rooms.map((room) => (
+                      <option key={room.id} value={room.type}>
+                        &nbsp;{room.type}
+                      </option>
+                    ))}
+                  </select>
+                </label>
+                <h1 className='text-white mt-2 mb-1'>Are you getting a mealplan:</h1>
+                <label className="flex text-med justify-start h-[2rem]">
+                  <select
+                    className="font-medium rounded-lg bg-gray-300 text-gray-500"
+                    onChange={handleMealplanChange}
+                    id="select_mealplan"
+                    value={mealplan}
+                  >
+                    <option value="" disabled hidden>&nbsp;Mealplan option</option>
+                    {mealplans.map((mealplan) => (
+                      <option key={mealplan.id} value={mealplan.name}>
+                        &nbsp;{mealplan.name}
+                      </option>
+                    ))}
+                  </select>
+                </label>
+              <h1 className='mt-2 mb-1 text-white'>What are some other amenities you wish to have? (Ex. gym, gaming room, great hall, pool table, ping pong table, etc...)</h1>
+              <textarea 
+                value={amenities}
+                onChange={handleAmenitiesChange}
                 className="w-full p-2 mt-2 border text-black border-gray-300 rounded-md bg-gray-300 focus:outline-none"
                 placeholder="Describe your ideal residence...">
-              </input>
-              <div className="mt-4">
-                <Button
-                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
-                  onClick={handleSubmit}
-                >
-                  Find my rez!
-                </Button>
-              </div>
+              </textarea>
+              <h1 className='mt-2 mb-1 text-white'>Other details (Ex. living learning community, close to UW plaza, etc...)</h1>
+              <textarea
+                value={details}
+                onChange={handleDetailsChange}
+                className="w-full p-2 mt-2 border text-black border-gray-300 rounded-md bg-gray-300 focus:outline-none"
+                placeholder="Describe your ideal residence...">
+              </textarea>
+              {isLoading ? (
+                <div className="mt-4">
+                  <span className="loading loading-spinner text-info"></span>
+                </div>
+                ) : (
+                <div className="mt-4">
+                  <Button
+                    className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
+                    onClick={handleFindResidences}
+                  >
+                    Find my rez!
+                  </Button>
+                </div>
+              )}
             </DialogPanel>
           </div>
         </div>
       </Dialog>
     </label>
+    <ResultDialog
+      ideal_residence={idealRes}
+      close={setIsResultOpen}
+      match={similarity}
+      isOpen={isResultOpen}
+    />
+  </>
   );
 };
 
